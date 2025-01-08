fa5aacf55eec4714e06b6ad68959d818b749a8ca
oguzhanulu <oguzhan.ulu@outlook.com>
2025-01-08 11:12:42 +0300

    Add Pagetitle and bug fixes

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,65 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Admin.css';
+import Sidebar from './Sidebar'; // Sidebar importu
+import PageTitle from '../PageTitle';
 
 function Admin() {
-  const [activeTab, setActiveTab] = useState('admin');
-
-  return (
-    <>
-      <div className="admin-panel">
-        {/* Sidebar */}
-        <aside className="sidebar">
-          <h2>BeeonHive</h2>
-          <nav>
-            <ul>
-              <li
-                className={activeTab === 'admin' ? 'active' : ''}
-                onClick={() => setActiveTab('admin')}
-              >
-                Admin Paneli
-              </li>
-              <li
-                className={activeTab === 'products' ? 'active' : ''}
-                onClick={() => setActiveTab('products')}
-              >
-                Ürünlere Göz At
-              </li>
-              <li
-                className={activeTab === 'contact' ? 'active' : ''}
-                onClick={() => setActiveTab('contact')}
-              >
-                İletişim
-              </li>
-              <li
-                className={activeTab === 'logout' ? 'active' : ''}
-                onClick={() => setActiveTab('logout')}
-              >
-                Çıkış Yap
-              </li>
-            </ul>
-          </nav>
-        </aside>
-
-        {/* Main Content */}
-        <main className="main-content">
-          <div className="stats-container">
-            <div className="stat-box">
-              <h3>Toplam Ürün</h3>
-              <p>0</p>
-            </div>
-            <div className="stat-box">
-              <h3>Toplam İleti</h3>
-              <p>0</p>
-            </div>
-            <div className="stat-box">
-              <h3>Toplam Kullanıcı</h3>
-              <p>0</p>
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
-  );
+    return (
+        <>
+        <PageTitle title='Dashboard'/>
+        <div className="admin-panel">
+            <Sidebar />  {/* Sidebar burada kullanılıyor */}
+            {/* Main Content */}
+            <main className="main-content">
+                <div className="stats-container">
+                    <div className="stat-box">
+                        <h3>Toplam Ürün</h3>
+                        <p>0</p>
+                    </div>
+                    <div className="stat-box">
+                        <h3>Toplam İleti</h3>
+                        <p>0</p>
+                    </div>
+                    <div className="stat-box">
+                        <h3>Toplam Kullanıcı</h3>
+                        <p>0</p>
+                    </div>
+                </div>
+            </main>
+        </div>
+        </>
+    );
 }
 
 export default Admin;
--- src/components/Adminmessages.js
+++ src/components/Adminmessages.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Adminmessages.css'; // AdminMessages için özel CSS
 import Sidebar from './Sidebar'; // Sidebar bileşenini içe aktar
+import PageTitle from '../PageTitle';
 
 function AdminMessages() {
     return (
+        <>
+        <PageTitle title='Gelen Mesajlar'/>
         <div className="admin-panel">
             <Sidebar /> {/* Sidebar'ı buraya ekleyin */}
             {/* Main Content */}
@@ -14,6 +17,7 @@ function AdminMessages() {
                 </div>
             </main>
         </div>
+        </>
     );
 }
 
